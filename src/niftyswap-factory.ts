fbd5a989efa868f7f86dcfe5064cc76d4edf4c75
sagostinelli <batonenbois@gmail.com>
2022-09-21 13:40:48 -0400

    block entity

--- src/niftyswap-factory.ts
+++ src/niftyswap-factory.ts
@@ -21,6 +21,10 @@ import {
   fetchCurrencyName,
   fetchCurrencySymbol,
 } from "./utils/currency";
+import { EthereumBlock } from '@graphprotocol/graph-ts'
+import { 
+  Block
+} from '../generated/schema'
 
 export function handleNewExchange(event: NewExchange): void {
   // Loading the Factory contract
@@ -96,4 +100,23 @@ export function handleOwnershipTransferred(event: OwnershipTransferred): void {
   factory.txCount = factory.txCount.plus(BigInt.fromI32(1));
   factory.owner = event.params.newOwner.toHex();
   factory.save();
+}
+
+export function handleBlock(block: EthereumBlock): void {
+  let id = block.hash.toHex()
+  let blockEntity = new Block(id);
+  blockEntity.number = block.number;
+  blockEntity.timestamp = block.timestamp;
+  blockEntity.parentHash = block.parentHash.toHex();
+  blockEntity.author = block.author.toHex();
+  blockEntity.difficulty = block.difficulty;
+  blockEntity.totalDifficulty = block.totalDifficulty;
+  blockEntity.gasUsed = block.gasUsed;
+  blockEntity.gasLimit = block.gasLimit;
+  blockEntity.receiptsRoot = block.receiptsRoot.toHex();
+  blockEntity.transactionsRoot = block.transactionsRoot.toHex();
+  blockEntity.stateRoot = block.stateRoot.toHex();
+  blockEntity.size = block.size;
+  blockEntity.unclesHash = block.unclesHash.toHex();
+  blockEntity.save();
 }
